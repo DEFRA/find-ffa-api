198a4d056084c8248eda6bf23a75e27ddeca30f5
Steve Dickinson <steve.dickinson@defra.gov.uk>
2024-11-19 09:00:07 +0000

    Update proxy

--- src/services/ai/clients/openai.js
+++ src/services/ai/clients/openai.js
@@ -3,35 +3,39 @@ import { proxyAgent } from '~/src/helpers/proxy-agent.js'
 
 import { config } from '~/src/config/index.js'
 
-const embeddings = new OpenAIEmbeddings({
-  azureOpenAIApiInstanceName: config.get('openAi.instanceName'),
-  azureOpenAIApiKey: config.get('openAi.key'),
-  azureOpenAIApiDeploymentName: config.get('openAi.embeddingModel'),
-  azureOpenAIApiVersion: config.get('openAi.apiVersion'),
-  onFailedAttempt: (error) => {
-    if (error.retriesLeft) {
-      throw new Error(`Failed to get OpenAI embeddings: ${error}`)
+const embeddings = new OpenAIEmbeddings(
+  {
+    azureOpenAIApiInstanceName: config.get('openAi.instanceName'),
+    azureOpenAIApiKey: config.get('openAi.key'),
+    azureOpenAIApiDeploymentName: config.get('openAi.embeddingModel'),
+    azureOpenAIApiVersion: config.get('openAi.apiVersion'),
+    onFailedAttempt: (error) => {
+      if (error.retriesLeft) {
+        throw new Error(`Failed to get OpenAI embeddings: ${error}`)
+      }
     }
   },
-  configuration: {
+  {
     baseURL: proxyAgent()?.url
   }
-})
+)
 
-const openai = new ChatOpenAI({
-  azureOpenAIApiInstanceName: config.get('openAi.instanceName'),
-  azureOpenAIApiKey: config.get('openAi.key'),
-  azureOpenAIApiDeploymentName: config.get('openAi.generationModel'),
-  azureOpenAIApiVersion: config.get('openAi.apiVersion'),
-  onFailedAttempt: (error) => {
-    if (error.retriesLeft) {
-      throw new Error(`Failed to query Azure OpenAI: ${error}`)
-    }
+const openai = new ChatOpenAI(
+  {
+    azureOpenAIApiInstanceName: config.get('openAi.instanceName'),
+    azureOpenAIApiKey: config.get('openAi.key'),
+    azureOpenAIApiDeploymentName: config.get('openAi.generationModel'),
+    azureOpenAIApiVersion: config.get('openAi.apiVersion'),
+    onFailedAttempt: (error) => {
+      if (error.retriesLeft) {
+        throw new Error(`Failed to query Azure OpenAI: ${error}`)
+      }
+    },
+    verbose: true
   },
-  verbose: true,
-  configuration: {
+  {
     baseURL: proxyAgent()?.url
   }
-})
+)
 
 export { openai, embeddings }
